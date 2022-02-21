34d36ad0c6d663f8a526b0d62c9cd71f5adc6c34
byeongdolee <bdbdbdlee@gmail.com>
2022-02-21 22:55:19 +0900

    로그인 디자인 조정

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,15 +2,14 @@
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.amazonaws.auth.AWSStaticCredentialsProvider
@@ -18,8 +17,8 @@ import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.model.Bucket
+import com.amazonaws.services.s3.model.S3ObjectSummary
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -47,56 +46,86 @@ fun App() {
         var accessKey by remember { mutableStateOf("") }
         var secretKey by remember { mutableStateOf("") }
         var buckets by remember { mutableStateOf(listOf<Bucket>()) }
-        Row {
-            Column {
-                OutlinedTextField(value = accessKey, onValueChange = { accessKey = it }, label = { Text("Access Key") })
-                OutlinedTextField(value = secretKey, onValueChange = { secretKey = it }, label = { Text("Secret Key") })
-                Button(
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = color,
-                        contentColor = Color.White,
-                        disabledBackgroundColor = Color.Gray
-                    ),
-                    enabled = !submitting,
-                    onClick = {
-                        scope.launch {
-                            submitting = true
-                            buckets = withContext(Dispatchers.Default) {
-                                fetchBuckets(accessKey, secretKey)
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier.padding(100.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.Center)
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.align(Alignment.CenterHorizontally).fillMaxWidth(),
+                    maxLines = 1,
+                    value = accessKey,
+                    onValueChange = { accessKey = it },
+                    label = { Text("Access Key") })
+                OutlinedTextField(
+                    modifier = Modifier.align(Alignment.CenterHorizontally).fillMaxWidth(),
+                    maxLines = 1,
+                    value = secretKey,
+                    onValueChange = { secretKey = it },
+                    label = { Text("Secret Key") })
+                Box(modifier = Modifier.align(Alignment.End)) {
+                    Button(
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = color,
+                            contentColor = Color.White,
+                            disabledBackgroundColor = Color.Gray
+                        ),
+                        enabled = !submitting,
+                        onClick = {
+                            scope.launch {
+                                submitting = true
+                                buckets = withContext(Dispatchers.Default) {
+                                    fetchBuckets(accessKey, secretKey)
+                                }
+                                submitting = false
                             }
-                            submitting = false
-                        }
-                    }) {
-                    Text(text = submitButtonTitle)
-                }
-            }
-            Column {
-                buckets.forEach {
-                    @OptIn(ExperimentalFoundationApi::class)
-                    Box(modifier = Modifier.combinedClickable(onDoubleClick = {
-                        println(it.name)
-                    }, onClick = {})) {
-                        Text(it.name)
+                        }) {
+                        Text(text = submitButtonTitle)
                     }
                 }
             }
+
+//            Column {
+//                buckets.forEach {
+//                    @OptIn(ExperimentalFoundationApi::class)
+//                    Box(modifier = Modifier.combinedClickable(onDoubleClick = {
+//                        val objects = scope.launch {
+//                            fetchObjects(accessKey, secretKey, it.name)
+//                        }
+//                        println(objects)
+//                    }, onClick = {})) {
+//                        Text(it.name)
+//                    }
+//                }
+//            }
         }
-
-
     }
 }
 
-suspend fun fetchBuckets(accessKey: String, secretKey: String): List<Bucket> {
+fun fetchBuckets(accessKey: String, secretKey: String): List<Bucket> {
+    try {
+        val credentials = BasicAWSCredentials(accessKey, secretKey)
+        val client = AmazonS3ClientBuilder.standard().withCredentials(AWSStaticCredentialsProvider(credentials))
+            .withRegion(Regions.AP_NORTHEAST_2)
+            .build()
+
+        return client.listBuckets()
+    } catch (e: Exception) {
+        println(e)
+    }
+
+    return emptyList()
+}
+
+fun fetchObjects(accessKey: String, secretKey: String, bucketName: String): List<S3ObjectSummary> {
     val credentials = BasicAWSCredentials(accessKey, secretKey)
     val client = AmazonS3ClientBuilder.standard().withCredentials(AWSStaticCredentialsProvider(credentials))
         .withRegion(Regions.AP_NORTHEAST_2)
         .build()
 
-    val buckets = coroutineScope {
-        client.listBuckets()
-    }
-
-    return buckets
+    val result = client.listObjectsV2(bucketName)
+    return result.objectSummaries
 }
 
 fun main() = application {
